545cc7be18ae0e9b2d54415654e2df7a082e077a
sandratra08 <45383481+sandratra08@users.noreply.github.com>
2023-11-29 17:02:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, query_param: str = None):
-    return {"item_id": item_id, "query_param": query_param}
+@app.get("/martial")
+def read_root():
+    return {"Hello": "Martial"}
